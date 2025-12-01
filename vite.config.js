47e14ebf07193aaa4f20bb69660f9bc95e85ead7
baggan <navilla.bagga@gmail.com>
2025-12-01 11:16:42 -0800

    updated billing

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
   build: {
-    outDir: "dist",
-  },
-};
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        success: "success.html",
+        cancel: "cancel.html"
+      }
+    }
+  }
+});
