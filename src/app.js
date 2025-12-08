62a2985570f55f6d59ff6e10227ef0f40a265c0b
baggan <navilla.bagga@gmail.com>
2025-12-08 15:37:04 -0800

    Revert "Update app.js"
    This reverts commit d2cbf5734ecd44e5b2b94646d674c6bed491a140.

--- src/app.js
+++ src/app.js
@@ -191,22 +191,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const { data } = await sb.auth.getSession();
 
     if (data.session) {
-		session = data.session;
-		accessToken = session.access_token;
-		userEmail = session.user.email;
+      session = data.session;
+      accessToken = session.access_token;
+      userEmail = session.user.email;
 
-		localStorage.setItem("nani_access_token", accessToken);
-		localStorage.setItem("nani_user_email", userEmail);
+      localStorage.setItem("nani_access_token", accessToken);
+      localStorage.setItem("nani_user_email", userEmail);
 
-	try {
-		await loadUserContext();   // ✅ ADD THIS
-		showScreen(true);
-	} catch (e) {
-		console.error("Session bootstrap failed", e);
-		showScreen(false);
-	}
-  }	
- }
+      showScreen(true);
+    } else {
+      showScreen(false);
+    }
+  }
 
   loadSession();
 
